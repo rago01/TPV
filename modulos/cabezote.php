f3fdec6aae1dc5a2ce4f4cd5efc31fc4f3f9c854
Oscar Garcia <ragogarcia1@gmail.com>
2020-04-12 00:08:06 -0500

    Iniciando a trabajar con plantilla Admin LTE

---
+++ modulos/cabezote.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+
+?>
+
+<header class="main-header">
+    <a href="" class="logo">
+        <!-- Logo mini -->
+        <span class="logo-mini">
+            <img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive"
+            alt="" style="padding: 10px">
+        </span>
+        <!-- Logo normal -->
+        <span class="logo-lg">
+            <img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive"
+            alt="" style="padding: 10px">
+        </span>
+    </a>
+    <nav class="navbar navbar-static-top" role="navigation">
+    <!--BOTON MENU HAMBURGUESA-->
+        <a href="" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Navigation</span>
+        </a>
+    <!--PERFIL DEL USUARIO-->
+    <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+            <li class="dropdown user user-menu">
+                <a href="" class="dropdown-toggle" data-toggle="dropdown">
+                    <img src="vistas/img/usuarios/default/anonymous.png" class="user-image" alt="">
+                    <span class="hidden-xs">Usuario Administrados</span>
+                </a>
+                <!--DROPDOWN TOGGLE-->
+                <ul class="dropdown-menu">
+                    <li class="user-body">
+                        <div class="pull-right">
+                            <a href="" class="btn btn-default btn-flat">Salir</a>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    
+    </nav>
+</header>
---
+++ modulos/menu.php
@@ -0,0 +1,75 @@
+<?php 
+?>
+
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <ul class="sidebar-menu">
+            <li class="active">
+                <a href="">
+                    <i class="fa fa-home"></i>
+                    <span>Inicio</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-user"></i>
+                    <span>Personas</span>
+                </a>
+            </li>
+            <li class="treeview">
+                <a href="">
+                    <i class="fa fa-list-ul"></i>
+                    <span>Productos</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Categorias</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Producto</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Movimientos inventario</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Proveedores</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Ingredientes</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Unidades</span>
+                        </a>
+                    </li>
+                    
+                </ul>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fa fa-dashboard"></i>
+                    <span>Reportes</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+</aside>
