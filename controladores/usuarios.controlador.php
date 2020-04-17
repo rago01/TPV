28724cf577ad1e1ff243c29c62b0a2901c3a78c9
Oscar Garcia <ragogarcia1@gmail.com>
2020-04-17 11:09:52 -0500

    Login

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -5,9 +5,9 @@ class ControladorUsuarios{
     public function ctrIngresoUsuario(){
         if (isset($_POST["user"])){
           if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['user']) && preg_match('/^[a-zA-Z0-9]+$/', $_POST['clave'])) {
-            echo  $tabla = "users";
-            echo  $item = "doc";
-            echo  $valor = $_POST['user'];
+             $tabla = "users";
+             $item = "doc";
+             $valor = $_POST['user'];
               $respuesta = ModeloUsuarios::mdlMostrarUsuarios($tabla,$item,$valor);
 
               echo var_dump($respuesta);
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
 require_once "controladores/plantilla.controlador.php";
-
+require_once "controladores/usuarios.controlador.php";
+require_once "modelos/usuarios.modelo.php";
 $plantilla = new ControladorPlantilla();
 $plantilla ->ctrPlantilla();
 ?>
