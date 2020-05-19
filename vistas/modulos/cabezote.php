51a856df3f820fa10b2f037c199191b5a2eff7f9
analista <ragogarcia1@gmail.com>
2020-05-19 16:53:51 -0500

    Modificando menu dropdown del perfil de usuario

--- vistas/modulos/cabezote.php
+++ vistas/modulos/cabezote.php
@@ -1,6 +1,19 @@
 <?php
-
-
+if ($_SESSION['id_perfil'] == "1") {
+    $tipoUsuario = "Administrador";
+}
+if ($_SESSION['id_perfil'] == "2") {
+    $tipoUsuario = "Cajero";
+}
+if ($_SESSION['id_perfil'] == "3") {
+    $tipoUsuario = "Mesero";
+}
+if ($_SESSION['id_perfil'] == "4") {
+    $tipoUsuario = "Cliente";
+}
+if ($_SESSION['id_perfil'] == "5") {
+    $tipoUsuario = "Cocinero";
+}
 
 ?>
 
@@ -22,20 +35,32 @@
     <!--PERFIL DEL USUARIO-->
     <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-            <li class="dropdown user user-menu">
-                <a href="" class="dropdown-toggle" data-toggle="dropdown">
-                    <img src="vistas/img/usuarios/default/anonymous.png" class="user-image" alt="">
-                    <span><?php echo $_SESSION['nombres'].' '.$_SESSION['apellidos']?></span>
-                </a>
-                <!--DROPDOWN TOGGLE-->
-                <ul class="dropdown-menu">
-                    <li class="">
-                        <div class="pull-right">
-                            <a href="logout" class="btn btn-default">Salir</a>
-                        </div>
-                    </li>
-                </ul>
+          <li class="dropdown user user-menu">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="vistas/img/usuarios/default/anonymous.png" class="user-image" alt="User Image">
+            <?php echo $_SESSION['nombres'].' '.$_SESSION['apellidos'] ?>
+          </a>
+          <ul class="dropdown-menu">
+            <!-- User image -->
+            <li class="user-header">
+              <img src="vistas/img/usuarios/default/anonymous.png" class="img-circle" alt="User Image">
+
+              <p>
+                <?php echo $_SESSION['nombres'].' '.$_SESSION['apellidos'] ?>
+                <small><?php echo $tipoUsuario; ?></small>
+              </p>
             </li>
+            <!-- Menu Footer-->
+            <li class="user-footer">
+              <div class="pull-left">
+                <a href="mi_perfil" class="btn btn-default"><span> <i class="fa fa-user"></i> Profile</a>
+              </div>
+              <div class="pull-right">
+                <a href="logout" class="btn btn-default btn-flat"> <span> <i class="fa fa-circle-o-notch"></i> </span> Salir</a>
+              </div>
+            </li>
+          </ul>
+        </li>
         </ul>
     </div>
 
