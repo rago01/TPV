6ab8243ecb96b0cc546daceaecb63140b4db8640
rrrago1 <51706994+rrrago1@users.noreply.github.com>
2021-06-20 18:39:26 -0500

    update

--- vistas/modulos/login.php
+++ vistas/modulos/login.php
@@ -29,7 +29,7 @@
       	<div class="modal-content">
 		<div class="modal-header text-center alert-danger" style="background: #3c8dbc; color:white;">
 			<button type="button" class="close" data-dismiss="modal">&times;</button>
-			<h4 class="modal-title">INGRESO CLIENTE</h4>
+			<h4 class="modal-title">INGRESO</h4>
 		</div>
 		<div class="modal-body">
 		      <div class="login-box-body">
@@ -48,31 +48,11 @@
 				 <button type="submit" class="btn btn-danger btn-block btn-flat" style="border-radius: 15px">Ingresar</button>
 			       </div>
 			     </div>
-			    <div class="modal-footer">
-			      <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
-			    </div>
 			</form>
 		      </div>
 		  </div>
 	   </div>
-      </div>
-  <!-- /.login-logo -->
-<!--  <div class="login-box-body">
-    <p class="login-box-msg">Ingresar al sistema</p>
-    <form method="post" onsubmit="return validar()">
-      <div class="form-group has-feedback">
-        <input type="number" class="form-control" placeholder="Usuario o Documento" required name="user">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Clave" name="clave1" required>
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row ">
-        <div class="col-xs-12 ">
-          <button type="submit" class="btn btn-warning btn-block btn-flat">Ingresar</button>
-        </div>
-      </div> -->
+</div>
       <?php
         include ("forms/login_user.php");
 
@@ -84,5 +64,3 @@
        //   $clienteLogin = new ControladorClientes();
        //   $clienteLogin -> ctrIngresoCliente();
       ?>
-
-<!-- /.login-box -->
