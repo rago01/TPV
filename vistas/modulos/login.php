963977ab01dee91a3d9a7d2d2198344f0a5a87d1
rrrago1 <51706994+rrrago1@users.noreply.github.com>
2021-06-20 18:29:28 -0500

    update

--- vistas/modulos/login.php
+++ vistas/modulos/login.php
@@ -24,6 +24,37 @@
   <div class="login-logo">
     <img src="vistas/img/plantilla/logo.png"  class="img-responsive" style="padding: 30px 100px 0px 100px ">
   </div>
+	
+	<div class="" id="" role="dialog">
+  <div class="modal-dialog">
+      <div class="modal-content">
+        <form role="form" method="post" enctype="multipart/form-darta" >
+          <div class="modal-header text-center alert-danger" style="background: #3c8dbc; color:white;">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">INGRESO CLIENTE</h4>
+          </div>
+  <div class="modal-body">
+        <div class="login-box-body">
+           <p class="login-box-msg">Ingresar al sistema</p>
+           <form method="post">
+             <div class="form-group has-feedback">
+               <input type="number" class="form-control" placeholder="Número celular" required name="userClient">
+               <span class="glyphicon glyphicon-user form-control-feedback"></span>
+             </div>
+             <div class="row ">
+               <div class="col-xs-12 ">
+                 <button type="submit" class="btn btn-danger btn-block btn-flat" style="border-radius: 15px">Ingresar</button>
+               </div>
+             </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+          </div>
+      </form>
+    </div>
+  </div>
+</div>
+</div>
 </div>
   <!-- /.login-logo -->
 <!--  <div class="login-box-body">
